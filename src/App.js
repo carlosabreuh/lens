69bbbeacf399998c0d7905abb01d7e2fb72b6c70
carlosabreuh <carlos@abreuh.com>
2021-01-31 15:35:31 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import './App.css';
 import SearchPhotos from './searchPhotos';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <h1 className="tittle">React Photo Search</h1>
-        <SearchPhotos />
-        <div>
-          <AmplifySignOut />
-          My App
+    <div>
+      <AmplifySignOut />
+      My App
+      <div className="App">
+        <div className="container">
+          <h1 className="tittle">React Photo Search</h1>
+          <SearchPhotos />
         </div>
       </div>
     </div>
