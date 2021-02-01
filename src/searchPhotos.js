e822ea127f4742bf9b01c5f0feec04a382be10a8
carlosabreuh <carlos@abreuh.com>
2021-02-01 09:16:46 -0500

    Login Background - Amplify Deploy

--- src/searchPhotos.js
+++ src/searchPhotos.js
@@ -11,17 +11,24 @@ const unsplash = new Unsplash({
 export default function SearchPhotos(keyword, page, per_page, filters) {
     const [query, setQuery] = useState(""); //Defining the State
     const [pics, setPics] = useState([]);
+    const [isExpanded, setExpand] = useState(false);
+
     
+    const expand = () => {
+        setExpand (!true);
+        console.log("clicked!");
+    }
 
     const searchPhotos = async (e) => {
         e.preventDefault();
         unsplash.search
-        .photos(query, 1, 50)
+        .photos(query, 1, 30)
         .then(toJson)
         .then((json) => {
             setPics(json.results);
            
-        });
+        })
+        //Haben's mess>>>>>>> .then(()=> unsplash.search.photos(query,2,30).then(toJson).then((json)=> {setPics([...pics,json.results])}))
         console.log("Submitting the Form")
     };
   return (
@@ -33,28 +40,33 @@ export default function SearchPhotos(keyword, page, per_page, filters) {
         </label>
         <input // This is our Search Field
           type="text" //The search input will be a string
-          name="query" 
+          name="query"
           className="input"
           placeholder={`Try "dog" or "apple"`}
           value={query}
           onChange={(e) => setQuery(e.target.value)} //This will update the state of our value
         />
-        <button type="submit" className="button">Search</button>
-        
-        
+        <button type="submit" className="button">
+          Search
+        </button>
       </form>
       <div className="card-list">
-          {
-          pics.map((pic) => <div className="card">
-              <img
-                className="card--image"
-                alt={pic.alt_description} //alt desctription of the image
-                src={pic.urls.full} //path of the image
-                width="50%"
-                height="50%"
-                ></img>
-          </div>)
-          }
+        {pics.map((pic) => (
+          <div className="card">
+            <img
+              className="card--image"
+              alt={pic.alt_description} //alt desctription of the image
+              src={pic.urls.small} //path of the image
+              width="50%"
+              height="50%"
+              onClick={expand}
+              
+
+             
+
+            ></img>
+          </div>
+        ))}
       </div>
     </>
   );
