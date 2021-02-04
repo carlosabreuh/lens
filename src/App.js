093294bd99bcee6c894d9c1bc194a1e920a70ba2
carlosabreuh <carlos@abreuh.com>
2021-02-03 23:32:47 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-// import Modal from './Modal'
+import React, { useState } from 'react';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import SearchPhotos from './searchPhotos';
 import Amplify from 'aws-amplify';
@@ -8,38 +8,78 @@ import config from './aws-exports';
 Amplify.configure(config);
 
 function App() {
-  /** Modals component */
-  // state = {
-  //   modal: false,
-  // };
-
-  // selectModal = (info) => {
-  //   this.setState({ modal: !this.state.modal }); // true/false toggle
-  // };
-  /**Modals component */
-  
-
+  const [likedPic, addToCollection] = useState([]);
   return (
+    <>
+      <Router>
+        <div className='topnav'>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/collection'>Collection</Link>
+            </li>
+          </ul>
+          <ul>
+            <AmplifySignOut />
+          </ul>
 
-    
-    <div className='App'>
-      {/* <p onClick={this.selectModal}>Open Modal</p>
-      <Modal />
-      displayModal={this.state.modal}
-      closeModal={this.selectModal} */}
-      
-    <div><AmplifySignOut /></div>
+          <hr />
 
-    
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+              <SearchPhotos
+                likedPic={likedPic}
+                addToCollection={addToCollection}
+              />
+            </Route>
+            <Route path='/about'>
+              <About />
+            </Route>
+            <Route path='/collection'>
+              <Collection
+                likedPic={likedPic}
+                addToCollection={addToCollection}
+              />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
 
-      <div className='container'>
-        {/* <div className='signout'> */}
-        
-
-        <h2 className='tittle'>React Photo Search</h2>
-        <SearchPhotos />
+      <div className='App'>
+        <div className='container'>
+          <h2 className='tittle'>React Photo Search</h2>
+        </div>
       </div>
-      
+    </>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Collection({ likedPic, addToCollection }) {
+  return (
+    <div>
+      <h2>Collection</h2>
     </div>
   );
 }
