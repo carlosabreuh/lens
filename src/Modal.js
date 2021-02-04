4b14a16ea488193e07d3505de12441206204b499
carlosabreuh <carlos@abreuh.com>
2021-02-03 23:32:53 -0500

    Update Modal.js

--- src/Modal.js
+++ src/Modal.js
@@ -1,30 +1,21 @@
-import { Component } from "react";
+import { Component } from 'react';
 // import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
 
 const modalRoot = document.getElementById('modal');
 const element = document.createElement('div');
 
-
 class Modal extends Component {
-    componentDidMount(){
-        modalRoot.appendChild(element)
-    }
-    componentWillUnmount(){
-        modalRoot.removeChild(element)
-    }
-    render() {
-        return ReactDOM.createPortal(this.props.children, element)
-
-        
-    }
-        
-    
-
+  componentDidMount() {
+    modalRoot.appendChild(element);
+  }
+  componentWillUnmount() {
+    modalRoot.removeChild(element);
+  }
+  render() {
+    return ReactDOM.createPortal(this.props.children, element);
+  }
 }
-
-
 
 export default Modal;
 
