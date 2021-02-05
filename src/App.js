a236bc8a784ddb7772b8164a4ed9248d66a0afd2
carlosabreuh <carlos@abreuh.com>
2021-02-04 22:44:56 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ Amplify.configure(config);
 
 function App() {
   const [likedPic, addToCollection] = useState([]);
+  console.log('%c Thank you for stopping by! 😀  This page was built using React.js, React-Router along with JavaScript ES6 making an API call to Unsplash.🤓   ', 'color:blue; font-size:50px');
+  var style =
+    'color: tomato; background:#eee; -webkit-text-stroke: 1px black; font-size:30px;';
+  console.log('%cFeel free to search anything you like and save it to your personal collection.😎   The collection will live for the current session and removed once you log out.😅 -- The application is being hosted with AWS-Amplify, so do not worry about your email ending up in a dark hole🤨, its totally secure! 🤗🦾  Feel free to reach out to me for any questions or suggestions on how i can make this app better! carlos@abreuh.com🤜🤛 ', style);
+
   return (
     <>
       <AmplifySignOut />
